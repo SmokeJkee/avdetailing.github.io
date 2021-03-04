6ef983517223f78efa7ef61d33e065096bae9c41
SmokeJkee <65736529+SmokeJkee@users.noreply.github.com>
2021-03-04 04:28:36 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$('.menu-btn').on('click', function(e) {
+  e.preventDefault;
+  $(this).toggleClass('menu-btn_active');
+});
+
+$(document).ready(function(){
+  $('.carousel__inner').slick({
+    speed: 1000,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/l_arrow.png"></img></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/r_arrow.png"></img></button>',
+    /* responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          arrows: false,
+          dots: true,
+        }
+      }
+    ] */
+  });
+});
+
