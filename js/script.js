e37f2e82583258eb528b9913c08bc1393e4a30ee
SmokeJkee <65736529+SmokeJkee@users.noreply.github.com>
2021-03-08 17:57:51 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 $('.menu-btn').on('click', function(e) {
   e.preventDefault;
   $(this).toggleClass('menu-btn_active');
+  $('.menu-nav').toggleClass('menu-nav_active');
 });
+
 
 $(document).ready(function(){
   $('.carousel__inner').slick({
